7c43ea1654676ea952bef119baabec6d388e88cb
Manasi Suryavanshi <158078225+manasisuryavanshi99@users.noreply.github.com>
2024-11-14 14:11:36 -0500

    netrules-push

--- main.tf
+++ main.tf
@@ -40,7 +40,15 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  tags = local.tags
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.example.id]
+  }
+
+  tags = {
+    environment = "staging"
+  }
 }
 
 // SQL DB 
