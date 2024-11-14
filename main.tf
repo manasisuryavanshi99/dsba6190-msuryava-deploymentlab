9edfd2fa0f931102ea83d05b3d0d09b0a753dc64
Manasi Suryavanshi <158078225+manasisuryavanshi99@users.noreply.github.com>
2024-11-14 14:13:41 -0500

    netrule-push

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "azurerm_storage_account" "storage" {
   network_rules {
     default_action             = "Deny"
     ip_rules                   = ["100.0.0.1"]
-    virtual_network_subnet_ids = [azurerm_subnet.example.id]
+    virtual_network_subnet_ids = [azurerm_subnet.snet.id]
   }
 
   tags = {
